0ab01b1aecfc452e2585ed98aa8ac0914ece4c82
sansan0817 <126034611+sansan0817@users.noreply.github.com>
2023-04-29 03:43:08 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': 'e5e0653c47caee32f380bdcbd13642d389b65b37',
+	    'sonar.login': '322360b2027d0e725432b02d3951750af7c19b95',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
